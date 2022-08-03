9ff7f2ca84c414cd3d05bbda43e7aa2dafb2586a
Khai Chang <2335501+kchang4@users.noreply.github.com>
2022-08-03 02:57:46 -0500

    added vars and changed app source

--- app/main.tf
+++ app/main.tf
@@ -7,7 +7,8 @@ terraform {
 }
 
 module "app" {
-  source        = "../../app/module"
+  source        = "git@github.com:kchang4/app.git//module?ref=main"
+  bucket_name   = var.bucket_name
   create_bucket = var.create_bucket
   test_id       = var.test_id
 }
--- app/variables.tf
+++ app/variables.tf
@@ -1,7 +1,14 @@
 variable "create_bucket" {
-  type = string
+  description = "Flag to determine if s3 photos bucket should be created or not"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the photos s3 bucket"
+  type        = string
 }
 
 variable "test_id" {
-  type = string
+  description = "The unique test string id to append to each service/resource"
+  type        = string
 }
